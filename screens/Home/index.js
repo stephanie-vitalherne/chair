aebd62ddd1b559288d77219558bc1fdcf4aa94c9
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-07-30 02:21:16 -0400

    initial commit

---
+++ screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { COLORS, SIZES, FONTS, images, icons } from '../../constants';
+import { styles } from './styles';
+
+const Home = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ screens/ItemDetail/styles.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+import { COLORS, SIZES, FONTS } from '../../constants';
+
+const styles = StyleSheet.create({});
+
+export { styles };
