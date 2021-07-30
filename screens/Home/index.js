e20db4f89b10a8a07abb9368c0c8332d62ba7ba0
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-07-30 17:00:33 -0400

    title for home screen done

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,13 +1,57 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 import { COLORS, SIZES, FONTS, images, icons } from '../../constants';
 import { styles } from './styles';
 
 const Home = () => {
+  function renderHeader() {
+    return (
+      <View style={styles.headerContainer}>
+        <View style={styles.headerIcons}>
+          <TouchableOpacity
+            style={styles.headerBtn}
+            onPress={() => console.log('Menu')}>
+            <Image
+              resizeMode="contain"
+              source={icons.menu}
+              style={styles.menuBtn}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cart}
+            onPress={() => console.log('Cart')}>
+            <Image
+              resizeMode="contain"
+              source={icons.cart}
+              style={styles.menuBtn}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderTitle(title) {
+    return (
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Collection of</Text>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      {renderHeader()}
+      {renderTitle('Test')}
+    </SafeAreaView>
   );
 };
 
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -1,6 +1,44 @@
+/* eslint-disable comma-dangle */
 import { StyleSheet } from 'react-native';
 import { COLORS, SIZES, FONTS } from '../../constants';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: COLORS.white
+  },
+  headerContainer: {
+    paddingHorizontal: SIZES.padding
+  },
+  headerIcons: {
+    flexDirection: 'row'
+  },
+  headerBtn: {
+    flex: 1
+  },
+  menuBtn: {
+    width: 25,
+    height: 25
+  },
+  cart: {
+    flex: 1,
+    alignItems: 'flex-end'
+  },
+  titleContainer: {
+    marginTop: SIZES.padding,
+    marginHorizontal: SIZES.padding
+  },
+  title: {
+    color: COLORS.black,
+    ...FONTS.largeTitle
+  }
+});
 
 export { styles };
